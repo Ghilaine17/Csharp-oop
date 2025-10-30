ae31140a72686b7fa7f4c2a1f28260d196d79d8a
Ghilaine17 <cardinalealessia3@gmail.com>
2025-10-30 09:09:58 +0100

    first commit

---
+++ utils/Product.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace utils
+{
+    public class Product
+    {
+        private int serialNumber;
+        public int SerialNumber
+        {
+            get
+            {
+                return serialNumber;
+            }
+            private set
+            {
+                serialNumber = value;
+            }
+        }
+        public string NameProduct { get; set; }
+        public string DescriptionProduct { get; set; }
+        public int PriceProduct { get; set; }
+        public int Tax { get; set; }
+
+        public Product(string nameProduct, string descriptionProduct, int priceProduct, int maxRandomNumber, int tax)
+        {
+            this.NameProduct = nameProduct;
+            this.DescriptionProduct = descriptionProduct;
+            this.PriceProduct = priceProduct;
+            this.SerialNumber = RandomNumberGenerator(maxRandomNumber);
+            this.Tax = tax;
+        }
+        private static int RandomNumberGenerator(int maxRandomNumber)
+        {
+            return new Random().Next(1, maxRandomNumber);
+        }
+
+        public string GetPriceWithTax(int price)
+        {
+            Tax = CheckNegativeNumber(Tax);
+            float priceWithTax = (price * ((float)Tax / 100)) + price;
+            return priceWithTax.ToString("0.00");
+        }
+
+        public string GetCompleteSerialNumber(int serialNumber)
+        {
+            char pad = '0';
+            string result = serialNumber.ToString().PadLeft(8, pad);
+            return result;
+        }
+
+        public void PrintCompleteName()
+        {
+            Console.WriteLine("Codice identificativo: "+ GetCompleteSerialNumber(SerialNumber) +"-"+ NameProduct);
+        }
+        public static int CheckNegativeNumber(int num)
+        {
+            if (num < 0)
+            {
+                Console.WriteLine("Hai inserito un numero negativo!");
+                return Math.Abs(num);
+            }
+            return num;
+        } 
+    }
+}
---
+++ utils/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Security.Cryptography;
+
+namespace utils
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Product product1 = new Product("Max Verstappen", "per chi vuole la massima velocità", 1200, 999999, -50);
+            Console.WriteLine("Nome prodotto: " + product1.NameProduct);
+            Console.WriteLine("Descrizione prodotto: " + product1.DescriptionProduct);
+            Console.WriteLine("Prezzo: " + product1.PriceProduct + " euro");
+            string priceWithTax = product1.GetPriceWithTax(product1.PriceProduct);
+            Console.WriteLine("Prezzo con iva: " + priceWithTax + " euro");
+            Console.WriteLine("Numero seriale completo: " + product1.GetCompleteSerialNumber(product1.SerialNumber));
+            product1.PrintCompleteName();
+            Console.WriteLine("");
+            List<Product> productList =
+                [
+                    new Product("Il Fabbricante di lacrime","Libro per adolescenti con copertina flessibile", 10, 8752,-18),
+                    new Product("Pallone da calcio arancione","Marca Santos", -10, 8752, 2),
+                    new Product("Orologio","Decorato in ottone", -30, 8752,43)
+                ];
+            foreach (Product product in productList)
+            {
+                Console.WriteLine("Nome prodotto: " + product.NameProduct);
+                Console.WriteLine("Descrizione prodotto: " + product.DescriptionProduct);
+                Console.WriteLine("Prezzo: " + Product.CheckNegativeNumber(product.PriceProduct) + " euro");
+                product.GetPriceWithTax(Product.CheckNegativeNumber(product.PriceProduct));
+                Console.WriteLine("Prezzo con iva: " + product.GetPriceWithTax(Product.CheckNegativeNumber(product.PriceProduct)) + " euro");
+                Console.WriteLine("Numero seriale completo: " + product.GetCompleteSerialNumber(product.SerialNumber));
+                product.PrintCompleteName();
+                Console.WriteLine("");
+
+            }
+        }
+    }
+    
+}
